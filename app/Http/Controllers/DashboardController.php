ef748f0441a8b797798d0b812989cfb155076c8c
officialGerero <vipskorpyon27@gmail.com>
2021-01-04 22:16:41 +0200

    Setting up database relations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Models\Subject;
 
 class DashboardController extends Controller
 {
     public function lol(){
-        $subjects = \App\Models\Subject::all();
-        return view('dashboard', compact('subjects'));
+        //$subjects = Subject::all();
+        $subjects = Subject::with('subjectToUserRel')->where('groupID', auth()->user()->getAuthIdentifier())->get();
+        return view('dashboard')->with('subjects', $subjects);
     }
 }
--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name_sub',
+        'name_teacher',
+        'groupID',
+    ];
+
+    public function subjectToUserRel(){
+        return $this->hasMany('\App\Models\User','id','groupID');
+    }
+
 }
