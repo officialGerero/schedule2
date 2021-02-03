70d2e8a0e5e22caecfc16c5284a38790cd948e94
Grenouillei <yulik15zaytpa@gmail.com>
2021-02-03 20:44:36 +0200

    test2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in! <br>
                     @foreach($subjects as $subject)
-                        {{ $subject->name_sub }} |
+                        {{ $subject->name_sub }} | deniskykoldinio
                         @foreach($subject->subjectToUserRel as $user)
                             {{ $user->name }}
                         @endforeach
