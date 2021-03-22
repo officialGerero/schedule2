44df47ee88260234d4e53238cd0a33705539f71e
officialGerero <vipskorpyon27@gmail.com>
2021-03-22 14:55:28 +0200

    Mobile tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,21 +21,17 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-900 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-300 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-                        {{-- @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif --}}
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-300 underline">Login</a>
                     @endif
                 </div>
             @endif
-            <h2 class="mt-2 text-gray-200">Hello page blyat</h2>
+            <h2 class="mt-2 text-gray-900 dark:text-gray-200">Hello page blyat</h2>
 
         </div>
     </body>
