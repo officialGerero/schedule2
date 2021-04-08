46b7b556fd5eae9585d089ea122d9d1c4a008372
officialGerero <vipskorpyon27@gmail.com>
2021-04-08 18:31:51 +0300

    Made changes so that web.php uses prefixes now

--- routes/web.php
+++ routes/web.php
@@ -22,28 +22,28 @@ Route::get('/dashboard',[Controllers\DashboardController::class, 'getSchedules']
 
 Route::middleware('admin')->group(function (){
 
-    Route::name('subject.')->group(function (){
-        Route::get('/subject/add',[Controllers\Admin\SubjectController::class,'getUsersInfo'])->name('view');
-        Route::post('/subject/add',[Controllers\Admin\SubjectController::class,'addSubject'])->name('add');
-        Route::put('/subject/edit/{id}',[Controllers\Admin\SubjectController::class,'updateSubject'])->name('edit');
-        Route::get('/subject/show/{id}',[Controllers\Admin\SubjectController::class, 'getSubject'])->name('prepare');
-        Route::get('/subject/delete/{id}',[Controllers\Admin\SubjectController::class, 'deleteSubject'])->name('delete');
+    Route::name('subject.')->prefix('subject')->group(function (){
+        Route::get('/add',[Controllers\Admin\SubjectController::class,'getUsersInfo'])->name('view');
+        Route::post('/add',[Controllers\Admin\SubjectController::class,'addSubject'])->name('add');
+        Route::put('/edit/{id}',[Controllers\Admin\SubjectController::class,'updateSubject'])->name('edit');
+        Route::get('/show/{id}',[Controllers\Admin\SubjectController::class, 'getSubject'])->name('prepare');
+        Route::get('/delete/{id}',[Controllers\Admin\SubjectController::class, 'deleteSubject'])->name('delete');
     });
 
-    Route::name('user.')->group(function (){
-        Route::view('/user/add','adduser')->name('view');
-        Route::post('/user/add',[Controllers\Admin\UserController::class, 'addUser'])->name('add');
-        Route::put('/user/edit/{id}',[Controllers\Admin\UserController::class, 'editUser'])->name('edit');
-        Route::get('/user/show/{id}',[Controllers\Admin\UserController::class, 'getUser'])->name('prepare');
-        Route::get('/user/delete/{id}',[Controllers\Admin\UserController::class, 'deleteUser'])->name('delete');
+    Route::name('user.')->prefix('user')->group(function (){
+        Route::view('/add','adduser')->name('view');
+        Route::post('/add',[Controllers\Admin\UserController::class, 'addUser'])->name('add');
+        Route::put('/edit/{id}',[Controllers\Admin\UserController::class, 'editUser'])->name('edit');
+        Route::get('/show/{id}',[Controllers\Admin\UserController::class, 'getUser'])->name('prepare');
+        Route::get('/delete/{id}',[Controllers\Admin\UserController::class, 'deleteUser'])->name('delete');
     });
 
-    Route::name('schedule.')->group(function (){
-        Route::get('/schedule/{id}',[Controllers\Admin\ScheduleController::class, 'prepareToAddScheduleId'])->name('view');
-        Route::post('/schedule',[Controllers\Admin\ScheduleController::class, 'addSchedule'])->name('add');
-        Route::put('/schedule/{id}',[Controllers\Admin\ScheduleController::class, 'editSchedule'])->name('edit');
-        Route::get('/schedule/show/{id}',[Controllers\Admin\ScheduleController::class, 'getSchedule'])->name('prepare');
-        Route::get('/schedule/delete/{id}',[Controllers\Admin\ScheduleController::class, 'deleteSchedule'])->name('delete');
+    Route::name('schedule.')->prefix('schedule')->group(function (){
+        Route::get('/{id}',[Controllers\Admin\ScheduleController::class, 'prepareToAddScheduleId'])->name('view');
+        Route::post('/',[Controllers\Admin\ScheduleController::class, 'addSchedule'])->name('add');
+        Route::put('/{id}',[Controllers\Admin\ScheduleController::class, 'editSchedule'])->name('edit');
+        Route::get('/show/{id}',[Controllers\Admin\ScheduleController::class, 'getSchedule'])->name('prepare');
+        Route::get('/delete/{id}',[Controllers\Admin\ScheduleController::class, 'deleteSchedule'])->name('delete');
     });
 
     Route::get('/users', [Controllers\ListsPageController::class,'users'])->name('users');
